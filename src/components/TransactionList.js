68ead27f1587f33713679f6dac3bd6a9785396dd
DygelnyyKirill <dygelniy.1994kirill@gmail.com>
2021-07-06 20:19:44 +0300

    AddTransaction component

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,14 +1,16 @@
+
+
 const TransactionList = () => {
     return (
         <>
             <h3>History</h3>
-            <ul id="list" className="list">
-                <li class="minus">
+            <ul className="list">
+                <li className="minus">
                     Cash <span>-400$</span>
                     <button className="delete-btn">x</button>
                 </li> 
             </ul>
-        </>
+        </> 
     )
 }
 
